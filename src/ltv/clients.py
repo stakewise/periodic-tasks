6d04367fcbeda52a9ad5f8456ad48078138380dc
Evgeny Gusarov <evgeny@stakewise.io>
2024-10-18 21:45:37 +0300

    Review fix

--- src/ltv/clients.py
+++ src/ltv/clients.py
@@ -19,7 +19,7 @@ logger = logging.getLogger(__name__)
 hot_wallet_account = Account().from_key(HOT_WALLET_PRIVATE_KEY)
 execution_client = Web3(Web3.HTTPProvider(EXECUTION_ENDPOINT))
 execution_client.middleware_onion.add(construct_sign_and_send_raw_middleware(hot_wallet_account))
-logger.info('hot wallet address: %s', hot_wallet_account.address)
+logger.info('Wallet address: %s', hot_wallet_account.address)
 
 
 def get_graph_client() -> gql.Client:
