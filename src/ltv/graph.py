1ed52393bbb336ef678e02cf68bdff2bdcbc8290
cyc60 <avsysoev60@gmail.com>
2024-11-21 12:16:23 +0300

    Update LTV all the osToken vaults.

---
+++ src/ltv/graph.py
@@ -0,0 +1,104 @@
+import logging
+
+from eth_typing import ChecksumAddress
+from gql import gql
+from hexbytes import HexBytes
+from web3 import Web3
+from web3.types import Wei
+
+from .clients import graph_client
+from .typings import HarvestParams
+
+logging.basicConfig(level=logging.INFO)
+logger = logging.getLogger(__name__)
+
+
+DEFAULT_NETWORK_ID = '0'
+
+
+def graph_get_ostoken_vaults() -> list[ChecksumAddress]:
+    query = gql(
+        """
+        query OsTokenVaultsIds($network: String) {
+          networks(
+            where: { id: $network }
+          ) {
+            osTokenVaultIds
+          }
+        }
+        """
+    )
+    params = {
+        'network': DEFAULT_NETWORK_ID,
+    }
+
+    response = graph_client.execute(query, params)
+    vaults = response['networks'][0]['osTokenVaultIds']  # pylint: disable=unsubscriptable-object
+    return [Web3.to_checksum_address(vault) for vault in vaults]
+
+
+def graph_get_vault_max_ltv_allocator(vault_address: str) -> ChecksumAddress | None:
+    query = gql(
+        """
+        query AllocatorsQuery($vault: String) {
+          allocators(
+            first: 1
+            orderBy: ltv
+            orderDirection: desc
+            where: { vault: $vault }
+          ) {
+            address
+          }
+        }
+        """
+    )
+    params = {
+        'vault': vault_address.lower(),
+    }
+
+    response = graph_client.execute(query, params)
+    allocators = response['allocators']  # pylint: disable=unsubscriptable-object
+
+    if not allocators:
+        return None
+
+    return Web3.to_checksum_address(allocators[0]['address'])
+
+
+def graph_get_harvest_params(vault_address: ChecksumAddress) -> HarvestParams | None:
+    query = gql(
+        """
+        query VaultQuery($vault: String) {
+          vault(
+            id: $vault
+          ) {
+            id
+            proof
+            proofReward
+            proofUnlockedMevReward
+            rewardsRoot
+          }
+        }
+        """
+    )
+    params = {
+        'vault': vault_address.lower(),
+    }
+
+    response = graph_client.execute(query, params)
+    vault_data = response['vault']  # pylint: disable=unsubscriptable-object
+
+    if (
+        vault_data['proof'] is None
+        or vault_data['proofReward'] is None
+        or vault_data['proofUnlockedMevReward'] is None
+        or vault_data['rewardsRoot'] is None
+    ):
+        return None
+
+    return HarvestParams(
+        rewards_root=HexBytes(Web3.to_bytes(hexstr=vault_data['rewardsRoot'])),
+        reward=Wei(int(vault_data['proofReward'])),
+        unlocked_mev_reward=Wei(int(vault_data['proofUnlockedMevReward'])),
+        proof=[HexBytes(Web3.to_bytes(hexstr=p)) for p in vault_data['proof']],
+    )
--- src/ltv/tasks.py
+++ src/ltv/tasks.py
@@ -1,16 +1,13 @@
 import logging
 from decimal import Decimal
 
-from eth_typing import ChecksumAddress
-from gql import gql
-from hexbytes import HexBytes
-from web3 import Web3
-from web3.types import Wei
-
-from .clients import execution_client, graph_client
+from .clients import execution_client
 from .contracts import vault_user_ltv_tracker_contract
-from .settings import VAULT
-from .typings import HarvestParams
+from .graph import (
+    graph_get_harvest_params,
+    graph_get_ostoken_vaults,
+    graph_get_vault_max_ltv_allocator,
+)
 
 logging.basicConfig(level=logging.INFO)
 logger = logging.getLogger(__name__)
@@ -25,115 +22,50 @@ def update_vault_max_ltv_user() -> None:
     Finds user having maximum LTV in given vault and submits this user in the LTV Tracker contract.
     """
     # Get max LTV user for vault
-    max_ltv_user = graph_get_vault_max_ltv_allocator(VAULT)
-    if max_ltv_user is None:
-        logger.warning('No allocators in vault')
-        return
-    logger.info('max LTV user for vault %s is %s', VAULT, max_ltv_user)
 
     block = execution_client.eth.get_block('finalized')
     logger.debug('Current block: %d', block['number'])
+    ostoken_vaults = graph_get_ostoken_vaults()
+    for vault in ostoken_vaults:
+        max_ltv_user = graph_get_vault_max_ltv_allocator(vault)
+        if max_ltv_user is None:
+            logger.warning('No allocators in vault')
+            continue
+        logger.info('max LTV user for vault %s is %s', vault, max_ltv_user)
 
-    harvest_params = graph_get_harvest_params(VAULT)
-    if not harvest_params:
-        logger.warning('No harvest params for vault %s', VAULT)
-        return
-    logger.debug('Harvest params: %s', harvest_params)
+        harvest_params = graph_get_harvest_params(vault)
+        if not harvest_params:
+            logger.warning('No harvest params for vault %s', vault)
+            continue
+        logger.debug('Harvest params: %s', harvest_params)
 
-    # Get current LTV
-    ltv = vault_user_ltv_tracker_contract.get_vault_max_ltv(VAULT, harvest_params)
-    logger.info('Current LTV: %s', Decimal(ltv) / WAD)
+        # Get current LTV
+        ltv = vault_user_ltv_tracker_contract.get_vault_max_ltv(vault, harvest_params)
+        logger.info('Current LTV: %s', Decimal(ltv) / WAD)
 
-    # Get prev max LTV user
-    prev_max_ltv_user = vault_user_ltv_tracker_contract.get_max_ltv_user(VAULT)
-    if max_ltv_user == prev_max_ltv_user:
-        logger.info('Max LTV user did not change since last update. Skip updating user.')
-        return
+        # Get prev max LTV user
+        prev_max_ltv_user = vault_user_ltv_tracker_contract.get_max_ltv_user(vault)
+        if max_ltv_user == prev_max_ltv_user:
+            logger.info('Max LTV user did not change since last update. Skip updating user.')
+            continue
 
-    # Update LTV
-    tx = vault_user_ltv_tracker_contract.update_vault_max_ltv_user(
-        VAULT, max_ltv_user, harvest_params
-    )
-    logger.info('Update transaction sent, tx hash: %s', tx.hex())
+        # Update LTV
+        tx = vault_user_ltv_tracker_contract.update_vault_max_ltv_user(
+            vault, max_ltv_user, harvest_params
+        )
+        logger.info('Update transaction sent, tx hash: %s', tx.hex())
 
-    # Wait for tx receipt
-    logger.info('Waiting for tx receipt')
-    receipt = execution_client.eth.wait_for_transaction_receipt(tx)
+        # Wait for tx receipt
+        logger.info('Waiting for tx receipt')
+        receipt = execution_client.eth.wait_for_transaction_receipt(tx)
 
-    # Check receipt status
-    if not receipt['status']:
-        raise RuntimeError(f'Update tx failed, tx hash: {tx.hex()}')
-    logger.info('Tx confirmed')
+        # Check receipt status
+        if not receipt['status']:
+            raise RuntimeError(f'Update tx failed, tx hash: {tx.hex()}')
+        logger.info('Tx confirmed')
 
-    # Get LTV after update
-    ltv = vault_user_ltv_tracker_contract.get_vault_max_ltv(VAULT, harvest_params)
-    logger.info('LTV after update: %s', Decimal(ltv) / WAD)
+        # Get LTV after update
+        ltv = vault_user_ltv_tracker_contract.get_vault_max_ltv(vault, harvest_params)
+        logger.info('LTV after update: %s', Decimal(ltv) / WAD)
 
     logger.info('Completed')
-
-
-def graph_get_vault_max_ltv_allocator(vault_address: str) -> ChecksumAddress | None:
-    query = gql(
-        """
-        query AllocatorsQuery($vault: String) {
-          allocators(
-            first: 1
-            orderBy: ltv
-            orderDirection: desc
-            where: { vault: $vault }
-          ) {
-            address
-          }
-        }
-        """
-    )
-    params = {
-        'vault': vault_address.lower(),
-    }
-
-    response = graph_client.execute(query, params)
-    allocators = response['allocators']  # pylint: disable=unsubscriptable-object
-
-    if not allocators:
-        return None
-
-    return Web3.to_checksum_address(allocators[0]['address'])
-
-
-def graph_get_harvest_params(vault_address: ChecksumAddress) -> HarvestParams | None:
-    query = gql(
-        """
-        query VaultQuery($vault: String) {
-          vault(
-            id: $vault
-          ) {
-            id
-            proof
-            proofReward
-            proofUnlockedMevReward
-            rewardsRoot
-          }
-        }
-        """
-    )
-    params = {
-        'vault': vault_address.lower(),
-    }
-
-    response = graph_client.execute(query, params)
-    vault_data = response['vault']  # pylint: disable=unsubscriptable-object
-
-    if (
-        vault_data['proof'] is None
-        or vault_data['proofReward'] is None
-        or vault_data['proofUnlockedMevReward'] is None
-        or vault_data['rewardsRoot'] is None
-    ):
-        return None
-
-    return HarvestParams(
-        rewards_root=HexBytes(Web3.to_bytes(hexstr=vault_data['rewardsRoot'])),
-        reward=Wei(int(vault_data['proofReward'])),
-        unlocked_mev_reward=Wei(int(vault_data['proofUnlockedMevReward'])),
-        proof=[HexBytes(Web3.to_bytes(hexstr=p)) for p in vault_data['proof']],
-    )
