e22866e6c0c3bffe24f7c58e9c2109aa1eb6b9b0
cyc60 <avsysoev60@gmail.com>
2024-11-22 15:36:39 +0300

    Review fixes

--- src/ltv/tasks.py
+++ src/ltv/tasks.py
@@ -1,6 +1,8 @@
 import logging
 from decimal import Decimal
 
+from eth_typing import ChecksumAddress
+
 from .clients import execution_client
 from .contracts import vault_user_ltv_tracker_contract
 from .graph import (
@@ -27,45 +29,46 @@ def update_vault_max_ltv_user() -> None:
     logger.debug('Current block: %d', block['number'])
     ostoken_vaults = graph_get_ostoken_vaults()
     for vault in ostoken_vaults:
-        max_ltv_user = graph_get_vault_max_ltv_allocator(vault)
-        if max_ltv_user is None:
-            logger.warning('No allocators in vault %s', vault)
-            continue
-        logger.info('max LTV user for vault %s is %s', vault, max_ltv_user)
-
-        harvest_params = graph_get_harvest_params(vault)
-        if not harvest_params:
-            logger.warning('No harvest params for vault %s', vault)
-            continue
-        logger.debug('Harvest params: %s', harvest_params)
-
-        # Get current LTV
-        ltv = vault_user_ltv_tracker_contract.get_vault_max_ltv(vault, harvest_params)
-        logger.info('Current LTV for vault %s: %s', vault, Decimal(ltv) / WAD)
-
-        # Get prev max LTV user
-        prev_max_ltv_user = vault_user_ltv_tracker_contract.get_max_ltv_user(vault)
-        if max_ltv_user == prev_max_ltv_user:
-            logger.info('Max LTV user did not change since last update. Skip updating user.')
-            continue
-
-        # Update LTV
-        tx = vault_user_ltv_tracker_contract.update_vault_max_ltv_user(
-            vault, max_ltv_user, harvest_params
-        )
-        logger.info('Update transaction sent, tx hash: %s', tx.hex())
-
-        # Wait for tx receipt
-        logger.info('Waiting for tx receipt')
-        receipt = execution_client.eth.wait_for_transaction_receipt(tx)
-
-        # Check receipt status
-        if not receipt['status']:
-            raise RuntimeError(f'Update tx failed, tx hash: {tx.hex()}')
-        logger.info('Tx confirmed')
-
-        # Get LTV after update
-        ltv = vault_user_ltv_tracker_contract.get_vault_max_ltv(vault, harvest_params)
-        logger.info('LTV for vault %s after update: %s', vault, Decimal(ltv) / WAD)
+        handle_vault(vault)
 
     logger.info('Completed')
+
+
+def handle_vault(vault: ChecksumAddress) -> None:
+    max_ltv_user = graph_get_vault_max_ltv_allocator(vault)
+    if max_ltv_user is None:
+        logger.warning('No allocators in vault %s', vault)
+        return
+    logger.info('max LTV user for vault %s is %s', vault, max_ltv_user)
+
+    harvest_params = graph_get_harvest_params(vault)
+    logger.debug('Harvest params for vault %s: %s', vault, harvest_params)
+
+    # Get current LTV
+    ltv = vault_user_ltv_tracker_contract.get_vault_max_ltv(vault, harvest_params)
+    logger.info('Current LTV for vault %s: %s', vault, Decimal(ltv) / WAD)
+
+    # Get prev max LTV user
+    prev_max_ltv_user = vault_user_ltv_tracker_contract.get_max_ltv_user(vault)
+    if max_ltv_user == prev_max_ltv_user:
+        logger.info('Max LTV user did not change since last update. Skip updating user.')
+        return
+
+    # Update LTV
+    tx = vault_user_ltv_tracker_contract.update_vault_max_ltv_user(
+        vault, max_ltv_user, harvest_params
+    )
+    logger.info('Update transaction sent, tx hash: %s', tx.hex())
+
+    # Wait for tx receipt
+    logger.info('Waiting for tx receipt')
+    receipt = execution_client.eth.wait_for_transaction_receipt(tx)
+
+    # Check receipt status
+    if not receipt['status']:
+        raise RuntimeError(f'Update tx failed, tx hash: {tx.hex()}')
+    logger.info('Tx confirmed')
+
+    # Get LTV after update
+    ltv = vault_user_ltv_tracker_contract.get_vault_max_ltv(vault, harvest_params)
+    logger.info('LTV for vault %s after update: %s', vault, Decimal(ltv) / WAD)
