4150f4ae819333dabe8a92151e1ab7f7ea8ff211
Alexander Sysoev <avsysoev60@gmail.com>
2024-12-26 11:54:44 +0300

    Add sentry (#6)

--- src/common/settings.py
+++ src/common/settings.py
@@ -5,5 +5,7 @@ from src.common.networks import NETWORKS, Network
 EXECUTION_ENDPOINT: str = config('EXECUTION_ENDPOINT', default='')
 HOT_WALLET_PRIVATE_KEY: str = config('HOT_WALLET_PRIVATE_KEY')
 
+SENTRY_DSN: str = config('SENTRY_DSN', default='')
+
 NETWORK: Network = config('NETWORK', cast=Network)
 network_config = NETWORKS[NETWORK]
--- src/update_price.py
+++ src/update_price.py
@@ -1,12 +1,14 @@
 import logging
 import sys
 
+from src.common.sentry import setup_sentry
 from src.price.tasks import check_and_sync
 
 logger = logging.getLogger(__name__)
 
 
 try:
+    setup_sentry()
     check_and_sync()
 except Exception as e:
     logger.error(e)
