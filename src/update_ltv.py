e1f09ae430ce9a6dd7593adf03f89086b94f279c
cyc60 <avsysoev60@gmail.com>
2024-12-26 11:32:36 +0300

    Add sentry

--- src/update_ltv.py
+++ src/update_ltv.py
@@ -1,11 +1,13 @@
 import logging
 import sys
 
+from src.common.sentry import setup_sentry
 from src.ltv.tasks import update_vault_max_ltv_user
 
 logger = logging.getLogger(__name__)
 
 try:
+    setup_sentry()
     update_vault_max_ltv_user()
 except Exception as e:
     logger.error(e)
