633e1fd989e482fd5bf7af9997a5424ddc63c1b7
evgeny-stakewise <123374581+evgeny-stakewise@users.noreply.github.com>
2025-01-06 14:15:36 +0300

    Del try except (#9)
    * Del redundant try-except
    
    * Add items to gitignore

--- src/update_price.py
+++ src/update_price.py
@@ -1,5 +1,4 @@
 import logging
-import sys
 
 from src.common.sentry import setup_sentry
 from src.price.tasks import check_and_sync
@@ -7,9 +6,5 @@ from src.price.tasks import check_and_sync
 logger = logging.getLogger(__name__)
 
 
-try:
-    setup_sentry()
-    check_and_sync()
-except Exception as e:
-    logger.error(e)
-    sys.exit(1)
+setup_sentry()
+check_and_sync()
