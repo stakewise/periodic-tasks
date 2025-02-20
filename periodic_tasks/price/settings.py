82290192ebfc93c1bd8bfb0f6e1e14d4558fab2e
cyc60 <avsysoev60@gmail.com>
2025-02-20 15:05:54 +0300

    Add sepolia to price update

--- periodic_tasks/price/settings.py
+++ periodic_tasks/price/settings.py
@@ -3,7 +3,7 @@ from typing import cast
 from decouple import config
 from sw_utils import MAINNET
 
-from periodic_tasks.common.networks import PRICE_NETWORKS, PriceNetworkConfig
+from periodic_tasks.common.networks import PRICE_NETWORKS, SEPOLIA, PriceNetworkConfig
 from periodic_tasks.common.settings import NETWORK
 
 sender_execution_endpoint: str = config('SENDER_EXECUTION_ENDPOINT')
@@ -12,4 +12,4 @@ target_execution_endpoint: str = config('TARGET_EXECUTION_ENDPOINT')
 price_network_config = cast(PriceNetworkConfig, PRICE_NETWORKS[NETWORK])
 
 
-SUPPORTED_NETWORKS = [MAINNET]
+SUPPORTED_NETWORKS = [MAINNET, SEPOLIA]
