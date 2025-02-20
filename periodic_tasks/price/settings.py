25bff252bda4e9cf45b143c7a7ca7af0d1d29784
Alexander Sysoev <avsysoev60@gmail.com>
2025-02-20 15:44:41 +0300

    Add supported networks check (#23)
    * Add supported networks check
    
    * Add sepolia to price update

--- periodic_tasks/price/settings.py
+++ periodic_tasks/price/settings.py
@@ -1,11 +1,15 @@
 from typing import cast
 
 from decouple import config
+from sw_utils import MAINNET
 
-from periodic_tasks.common.networks import PRICE_NETWORKS, PriceNetworkConfig
+from periodic_tasks.common.networks import PRICE_NETWORKS, SEPOLIA, PriceNetworkConfig
 from periodic_tasks.common.settings import NETWORK
 
 sender_execution_endpoint: str = config('SENDER_EXECUTION_ENDPOINT')
 target_execution_endpoint: str = config('TARGET_EXECUTION_ENDPOINT')
 
 price_network_config = cast(PriceNetworkConfig, PRICE_NETWORKS[NETWORK])
+
+
+SUPPORTED_NETWORKS = [MAINNET, SEPOLIA]
