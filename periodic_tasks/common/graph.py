8ed7f31f467078d41b3a4dcef2a1c42f776543f1
Evgeny Gusarov <evgeny@stakewise.io>
2025-09-03 19:35:44 +0300

    Refactor graph_get_vaults

--- periodic_tasks/common/graph.py
+++ periodic_tasks/common/graph.py
@@ -1,4 +1,3 @@
-import json
 import logging
 
 from eth_typing import ChecksumAddress
@@ -21,45 +20,49 @@ async def graph_get_vaults(
     """
     Returns mapping from vault address to Vault object
     """
-    where_conditions = []
+    where_conditions: list[str] = []
+    params: dict = {}
 
     if vaults:
-        where_conditions.append(f'id_in: {json.dumps([v.lower() for v in vaults])}')
+        where_conditions.append('id_in: $vaults')
+        params['vaults'] = [v.lower() for v in vaults]
 
     if is_meta_vault is not None:
-        where_conditions.append(f'isMetaVault: {json.dumps(is_meta_vault)}')
+        where_conditions.append('isMetaVault: $isMetaVault')
+        params['isMetaVault'] = is_meta_vault
 
     where_conditions_str = '\n'.join(where_conditions)
     where_clause = f'where: {{ {where_conditions_str} }}' if where_conditions else ''
 
-    query = gql(
-        f"""
-      query VaultQuery {{
-        vaults(
-          {where_clause}
-        ) {{
-          id
-          isMetaVault
-          subVaults {{
-            subVault
-          }}
-          canHarvest
-          proof
-          proofReward
-          proofUnlockedMevReward
-          rewardsRoot
-        }}
-      }}
-      """
-    )
-    params: dict = {}
+    filters = ['first: $first', 'skip: $skip']
 
-    response = await graph_client.run_query(query, params)
-    vault_data = response['vaults']  # pylint: disable=unsubscriptable-object
+    if where_clause:
+        filters.append(where_clause)
+
+    query = f"""
+        query VaultQuery($first: Int, $skip: Int, $vaults: [String], $isMetaVault: Boolean) {{
+            vaults(
+                {', '.join(filters)}
+            ) {{
+                id
+                isMetaVault
+                subVaults {{
+                    subVault
+                }}
+                canHarvest
+                proof
+                proofReward
+                proofUnlockedMevReward
+                rewardsRoot
+            }}
+        }}
+        """
+
+    response = await graph_client.fetch_pages(gql(query), params)
 
     graph_vaults_map: dict[ChecksumAddress, Vault] = {}
 
-    for vault_item in vault_data:
+    for vault_item in response:
         vault = _convert_vault_item_to_vault(vault_item)
         graph_vaults_map[vault.address] = vault
 
